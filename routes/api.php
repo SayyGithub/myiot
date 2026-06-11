efdfb1034fb9a9ea7fcee58a17d08a569819bc62
ammarsetiawan <ammarsetiawan970@gmail.com>
2026-06-11 17:12:11 +0700

    Mastiin API

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,12 @@
 <?php
-use App\Http\Controllers\Api\DeviceStatusController;
+
 use App\Http\Controllers\Api\SensorController;
+use App\Http\Controllers\Api\DeviceStatusController;
 use Illuminate\Support\Facades\Route;
 
+Route::post('/sensor', [SensorController::class, 'store']);
 Route::post('/device/status', [DeviceStatusController::class, 'update']);
 Route::get('/sensor', [SensorController::class, 'index']);
 Route::get('/sensor/{id}', [SensorController::class, 'show']);
-Route::post('/sensor', [SensorController::class, 'store']);
 Route::put('/sensor/{id}', [SensorController::class, 'update']);
 Route::delete('/sensor/{id}', [SensorController::class, 'destroy']);
